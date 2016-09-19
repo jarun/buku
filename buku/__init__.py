710896f7e46b4cdd5c84c476b8fd9e8191160b88
Ovv <ovv@outlook.com>
2016-09-19 22:50:26 +0200

    0.0.9 restructure for separate pypi branch

---
+++ buku/__init__.py
@@ -0,0 +1,1 @@
+from buku import buku
--- setup.py
+++ setup.py
@@ -3,21 +3,29 @@ from pip.req import parse_requirements
 from os import path
 from codecs import open
 
+with open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='buku',
-    version='0.0.5',
+    version='0.0.9',
     description='Powerful command-line bookmark manager. Your mini web!',
+    long_description=long_description,
     url='https://github.com/jarun/Buku',
-    author='Ovv',
+    author='Jarun',
     author_email='ovv@outlook.com',
     license='GPLv3',
-    packages=['buku', 'tests', 'auto-completion'],
-    zip_safe=False,
+    packages=['buku'],
     include_package_data=True,
-    package_data={'tests': ['ci-test-wrapper'],
-                  'auto-completion': ['bash/*', 'fish/*', 'zsh/*']},
+    package_data={'auto-completion': ['bash/*', 'fish/*', 'zsh/*']},
     entry_points={
         'console_scripts': ['buku=buku.buku:entry_point']
     },
     install_requires=['beautifulsoup4>=4.4.1', 'cryptography>=1.3.2'],
+    classifiers=['Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+                 'Programming Language :: Python :: 3.3',
+                 'Operating System :: POSIX :: Linux',
+                 'Natural Language :: English',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Development Status :: 5 - Production/Stable'],
 )
