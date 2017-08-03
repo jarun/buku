f56d64d34fa00d78c802efe2416322cb76d47451
Arun Prakash Jana <engineerarun@gmail.com>
2017-08-03 09:49:20 +0530

    Prepare for release v3.2

--- buku.py
+++ buku.py
@@ -37,7 +37,7 @@ import urllib3
 from urllib3.util import parse_url, make_headers
 import webbrowser
 
-__version__ = '3.1'
+__version__ = '3.2'
 __author__ = 'Arun Prakash Jana <engineerarun@gmail.com>'
 __license__ = 'GPLv3'
 
