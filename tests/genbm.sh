8feeeedaabafd487389b615b5ad9649f7c8f9078
Arun Prakash Jana <engineerarun@gmail.com>
2017-10-12 23:00:48 +0530

    Add bash scriptlet to auto-generate dummy records.
    Usage:
    
        genbm.sh n
    where, n = number of bookmarks to generate.
    
    The scriptlet works offline, no internet connection is required.
    
    Note: this scriptlet can be used in Travis CI environment.

---
+++ tests/genbm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Scriptlet to auto-generate Buku bookmarks
+# Usage: genbm.sh n
+#        where, n = number of records to generate
+#
+# Author: Arun Prakash Jana (engineerarun@gmail.com)
+
+if [ "$#" -ne 1 ]; then
+    echo "usage: genbm n"
+    exit 1
+fi
+
+count=0
+
+while [ $count -lt "$1" ]; do
+    url=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+    buku -a https://www.$url.com --title Dummy bookmark for testing. --tag auto-generated, dummy bookmark --comment Generated from the test script $count.
+    let count=count+1
+done
