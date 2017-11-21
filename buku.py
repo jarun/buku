0d60a58adbd122e0112c52958213a79c22a5de42
Arun Prakash Jana <engineerarun@gmail.com>
2017-11-21 19:43:08 +0530

    Auto-import bookmarks on FreeBSD

--- buku.py
+++ buku.py
@@ -2050,7 +2050,7 @@ class BukuDb:
 
         FF_BM_DB_PATH = None
 
-        if sys.platform.startswith('linux'):
+        if sys.platform.startswith('linux') or sys.platform.startswith('freebsd'):
             GC_BM_DB_PATH = '~/.config/google-chrome/Default/Bookmarks'
             CB_BM_DB_PATH = '~/.config/chromium/Default/Bookmarks'
 
