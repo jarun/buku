f9f5c82c4759aa321b4a82e2647fd3bb360e778d
Arun Prakash Jana <engineerarun@gmail.com>
2017-12-02 20:25:33 +0530

    Update py module version requirement

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 tests_require = [
-    'pytest-cov', 'pytest-catchlog', 'hypothesis>=3.7.0', 'pytest>=3.1.2', 'py>=1.4.33',
+    'pytest-cov', 'pytest-catchlog', 'hypothesis>=3.7.0', 'pytest>=3.1.2', 'py>=1.5.0',
     'beautifulsoup4==4.6.0', 'flake8>=3.4.1', 'pylint>=1.7.2'
 ],
 
