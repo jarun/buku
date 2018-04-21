28e382bb8430fec712abaa07ab639768a36aabc8
rachmadaniHaryono <foreturiga@gmail.com>
2018-04-21 10:20:25 +0800

    chg: dev: tag statistic

--- bukuserver/server.py
+++ bukuserver/server.py
@@ -435,31 +435,61 @@ def view_statistic():
     if not statistic_data or request.method == 'POST':
         all_bookmarks = bukudb.get_rec_all()
         netloc = [urlparse(x[1]).netloc for x in all_bookmarks]
+        tag_set = [x[3] for x in all_bookmarks]
+        tag_items = []
+        for tags in tag_set:
+            tag_items.extend([x.strip() for x in tags.split(',') if x.strip()])
+        tag_counter = Counter(tag_items)
         statistic_datetime = arrow.now()
         STATISTIC_DATA = {
             'datetime': statistic_datetime,
             'netloc': netloc,
+            'tag_counter': tag_counter,
         }
     else:
         netloc = statistic_data['netloc']
         statistic_datetime = statistic_data['datetime']
+        tag_counter = statistic_data['tag_counter']
+
     netloc_counter = Counter(netloc)
     unique_netloc_len = len(set(netloc))
     colors = [
         "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
         "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
         "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+    show_netloc_table = False
     if unique_netloc_len > len(colors):
         max_netloc_item = len(colors)
+        netloc_colors = colors
+        show_netloc_table = True
     else:
-        colors = colors[:unique_netloc_len]
+        netloc_colors = colors[:unique_netloc_len]
         max_netloc_item = unique_netloc_len
     most_common_netlocs = netloc_counter.most_common(max_netloc_item)
-    most_common_netlocs = [[val[0], val[1], colors[idx]] for idx, val in enumerate(most_common_netlocs)]
+    most_common_netlocs = [
+        [val[0], val[1], netloc_colors[idx]] for idx, val in enumerate(most_common_netlocs)]
+
+    unique_tag_len = len(tag_counter)
+    show_tag_rank_table = False
+    if unique_tag_len > len(colors):
+        max_tag_item = len(colors)
+        tag_colors = colors
+        show_tag_rank_table = True
+    else:
+        tag_colors = colors[:unique_tag_len]
+        max_tag_item = unique_tag_len
+    most_common_tags = tag_counter.most_common(max_tag_item)
+    most_common_tags = [
+        [val[0], val[1], tag_colors[idx]] for idx, val in enumerate(most_common_tags)]
+
     return render_template(
         'bukuserver/statistic.html',
         most_common_netlocs=most_common_netlocs,
         netloc_counter=netloc_counter,
+        show_netloc_table=show_netloc_table,
+        most_common_tags=most_common_tags,
+        tag_counter=tag_counter,
+        show_tag_rank_table=show_tag_rank_table,
         datetime=statistic_datetime,
         datetime_text=statistic_datetime.humanize(arrow.now(), granularity='second'),
     )
