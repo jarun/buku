c93e7785b9f6076551a4ecd5d0f7c322bc635827
rachmadaniHaryono <foreturiga@gmail.com>
2018-06-22 12:04:48 +0800

    chg: dev: sort required package

--- setup.py
+++ setup.py
@@ -16,10 +16,15 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 tests_require = [
-    'pytest-cov', 'hypothesis>=3.7.0', 'py>=1.5.0',
-    'beautifulsoup4>=4.6.0', 'flake8>=3.4.1', 'pylint>=1.7.2', 'PyYAML>=3.12',
-    'pytest>=3.4.2', 'attrs>=17.4.0',
-
+    'attrs>=17.4.0',
+    'beautifulsoup4>=4.6.0',
+    'flake8>=3.4.1',
+    'hypothesis>=3.7.0',
+    'py>=1.5.0',
+    'pylint>=1.7.2',
+    'pytest-cov',
+    'pytest>=3.4.2',
+    'PyYAML>=3.12',
 ]
 
 
