3aefb27319a8c9fbe9327ab7eb4b1e4e84991a3c
rachmadaniHaryono <foreturiga@gmail.com>
2018-06-28 20:06:51 +0800

    chg: dev: remove duplicate package

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ server_require = [
     'Flask-API>=0.6.9',
     'Flask-Bootstrap>=3.3.7.1',
     'flask-paginate>=0.5.1',
-    'Flask-WTF==0.14.2',
     'Flask-WTF>=0.14.2',
     'Flask>=0.12',
     'requests>=2.18.4',
