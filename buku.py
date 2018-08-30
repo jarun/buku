d54044c85e938bbf53c76e356a45651b8413cb88
Arun Prakash Jana <engineerarun@gmail.com>
2018-08-30 06:58:33 +0530

    Prepare for release v3.9

--- buku.py
+++ buku.py
@@ -44,7 +44,7 @@ from urllib3.exceptions import LocationParseError
 from urllib3.util import parse_url, make_headers
 import webbrowser
 
-__version__ = '3.8'
+__version__ = '3.9'
 __author__ = 'Arun Prakash Jana <engineerarun@gmail.com>'
 __license__ = 'GPLv3'
 
