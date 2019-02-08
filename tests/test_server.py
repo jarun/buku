7ff6994d9dfde44b5c13f2628ac5be92233a032c
rachmadaniHaryono <foreturiga@gmail.com>
2019-02-08 19:38:24 +0800

    new: test: simple test for bukuserver cli

---
+++ tests/test_server.py
@@ -0,0 +1,17 @@
+from click.testing import CliRunner
+import pytest
+
+from bukuserver import server
+
+@pytest.mark.parametrize(
+    'args,word',
+    [
+        ('--help', 'bukuserver'),
+        ('--version', 'Buku')
+    ]
+)
+def test_cli(args, word):
+    runner = CliRunner()
+    result = runner.invoke(server.cli, [args])
+    assert result.exit_code == 0
+    assert word in result.output
