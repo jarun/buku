04243242af4a7275efa663314ea377192e12657d
Arun Prakash Jana <engineerarun@gmail.com>
2019-10-16 23:19:05 +0530

    Python 3.8 is released

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
         'Topic :: Utilities'
     ]
