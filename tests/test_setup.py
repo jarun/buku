2b1c93a315653d70a8de66b175977f395907330c
Rachmadani Haryono <foreturiga@gmail.com>
2022-08-26 07:52:31 +0800

    test(bukuserver): update requirement check

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,10 +1,15 @@
+import pathlib
+
+
 def test_bukuserver_requirement(monkeypatch):
     def m_setup(**kwargs):
         return None
     import setuptools
     monkeypatch.setattr(setuptools, 'setup', m_setup)
     import setup
-    setup_l = setup.server_require
-    with open('bukuserver/requirements.txt', encoding="utf8", errors="surrogateescape") as f:
-        bs_l = [x for x in f.read().splitlines() if not x.startswith('buku')]
-    assert bs_l == setup_l
+
+    assert [
+        x
+        for x in pathlib.Path("bukuserver/requirements.txt").read_text(encoding="utf8", errors="surrogateescape").splitlines()
+        if "flask-reverse-proxy-fix" not in x
+    ] == setup.server_require
