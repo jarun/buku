d2651425cb4506a4b4a73afd94ed0d81605c38d4
LeXofLeviafan <lexofleviafan@gmail.com>
2024-10-20 21:23:20 +0200

    [jarun#780] added Python 3.13 + dropped Python 3.8

--- setup.py
+++ setup.py
@@ -104,11 +104,11 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
         'Topic :: Utilities'
     ]
