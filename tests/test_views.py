edff93cb060b1063535f3098c246e26783eefa86
LeXofLeviafan <lexofleviafan@gmail.com>
2025-04-12 22:19:06 +0200

    [jarun#828] added functional test for empty title/netloc

--- tests/test_views.py
+++ tests/test_views.py
@@ -302,7 +302,19 @@ def test_env_per_page(bukudb, app, client, total, per_page, pages, last_page):
 @pytest.mark.parametrize('favicons', [False, True, None])
 @pytest.mark.parametrize('mode', ['full', 'netloc', 'netloc-tag', None])
 def test_env_entry_render_params(bukudb, app, client, mode, favicons, new_tab):
-    url, netloc, title, desc, tags = 'http://example.com', 'example.com', 'Sample site', 'Foo bar baz', ',bar,baz,foo,'
+    _test_env_entry_render_params(bukudb, app, client, mode, favicons, new_tab, 'http://example.com', 'example.com', 'Sample site')
+
+@pytest.mark.parametrize('url, netloc, title', [
+    ('http://example.com', 'example.com', ''),
+    ('javascript:void(0)', '', 'Sample site'),
+    ('javascript:void(0)', '', ''),
+])
+@pytest.mark.parametrize('mode', ['full', 'netloc', 'netloc-tag'])
+def test_env_entry_render_params_blanks(bukudb, app, client, mode, url, netloc, title):
+    _test_env_entry_render_params(bukudb, app, client, mode, True, True, url, netloc, title)
+
+def _test_env_entry_render_params(bukudb, app, client, mode, favicons, new_tab, url, netloc, title):
+    desc, tags = 'Foo bar baz', ',bar,baz,foo,'
     _add_rec(bukudb, url, title, tags, desc)
     _tags = tags.strip(',').split(',')
     if mode:
@@ -315,14 +327,17 @@ def test_env_entry_render_params(bukudb, app, client, mode, favicons, new_tab):
     dom = assert_response(client.get('/bookmark/'), '/bookmark/')
     cell = ' '.join(etree.tostring(dom.xpath(f'//td{xpath_cls("col-entry")}')[0], encoding='unicode').strip().split())
     target = '' if not new_tab else ' target="_blank"'
-    icon = '' if not favicons else f'<img class="favicon" src="http://www.google.com/s2/favicons?domain={netloc}"/> '
-    prefix = f'<td class="col-entry"> {icon}<span class="title"><a href="{url}"{target}>{title}</a></span>'
+    icon = '' if not favicons else (netloc and f'<img class="favicon" src="http://www.google.com/s2/favicons?domain={netloc}"/> ')
+    urltext = title or '&lt;EMPTY TITLE&gt;'
+    _title = (urltext if not netloc else f'<a href="{url}"{target}>{urltext}</a>')
+    prefix = f'<td class="col-entry"> {icon}<span class="title">{_title}</span>'
     tags = [f'<a class="btn label label-default" href="/bookmark/?flt0_tags_contain={s}">{s}</a>' for s in _tags]
-    netloc_tag = ('' if mode == 'netloc' else
+    netloc_tag = ('' if mode == 'netloc' or not netloc else
                   f'<a class="btn label label-success" href="/bookmark/?flt0_url_netloc_match={netloc}">netloc:{netloc}</a>')
     suffix = f'<div class="tag-list">{netloc_tag}{"".join(tags)}</div><div class="description">{desc}</div> </td>'
     if mode == 'netloc':
-        assert cell == f'{prefix}<span class="netloc"> (<a href="/bookmark/?flt0_url_netloc_match={netloc}">{netloc}</a>)</span>{suffix}'
+        _netloc = netloc and f'<span class="netloc"> (<a href="/bookmark/?flt0_url_netloc_match={netloc}">{netloc}</a>)</span>'
+        assert cell == prefix + _netloc + suffix
     elif mode == 'netloc-tag':
         assert cell == prefix + suffix
     else:
